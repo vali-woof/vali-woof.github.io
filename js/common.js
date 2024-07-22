8d380c9fe99001514bc161f3e8c524ac1376a3f4
vali-woof <167765720+vali-woof@users.noreply.github.com>
2024-07-22 13:24:09 +0200

    js update relative links

--- js/common.js
+++ js/common.js
@@ -1,8 +1,8 @@
 var barks = [
-    new Audio('res/sounds/single-bark-of-a-dog-38780.mp3'),
-    new Audio('res/sounds/barking-3-47779.mp3'),
-    new Audio('res/sounds/dog-bark-179915.mp3'),
-    new Audio('res/sounds/barking-123909.mp3')
+    new Audio('/res/sounds/single-bark-of-a-dog-38780.mp3'),
+    new Audio('/res/sounds/barking-3-47779.mp3'),
+    new Audio('/res/sounds/dog-bark-179915.mp3'),
+    new Audio('/res/sounds/barking-123909.mp3')
 ];
 
 var bark = () => {
