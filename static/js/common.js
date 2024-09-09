e9ff0fb4f0371a432b542a81a609cd03e6d1ac73
vali-woof <167765720+vali-woof@users.noreply.github.com>
2024-09-09 23:58:01 +0200

    move to scss

--- static/js/common.js
+++ static/js/common.js
@@ -8,15 +8,3 @@ var barks = [
 var bark = () => {
     barks[Math.floor(Math.random() * barks.length)].play();
 };
-
-var randomTheme = () => {
-    var r = Math.random();
-    console.log('Setting random theme: ' + r);
-    if (r < 0.3)
-        document.getElementById('theme').className = 'theme-1';
-    else if (r >= 0.3 && r < 0.6)
-        document.getElementById('theme').className = 'theme-2';
-    else
-        document.getElementById('theme').className = 'theme-3';
-};
-// randomTheme();
